1cc50e2015c06c6af67afcfcedea789cfc1a1951
Dsh <knowslana@gmail.com>
2024-05-03 14:41:59 +0300

    модели задачи, решения

--- lesson/models.py
+++ lesson/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from lesson import const
 from utils.models import EditingModel, AuthoringModel
 
 
@@ -25,6 +26,21 @@ class Task(AuthoringModel):
     name = models.CharField(max_length=1024, null=False, blank=False, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
     block = models.ForeignKey(to=Block, verbose_name='Блок', null=False, on_delete=models.CASCADE)
+    correct_answer = models.TextField(verbose_name='Правильный ответ', null=True, blank=True)
+    max_mark = models.IntegerField('Максимальный балл')
+    need_file = models.BooleanField(verbose_name='Можно приложить файл', default=False)
+    autotest = models.BooleanField(verbose_name='Автоматическая проверка', default=True)
 
     def __str__(self):
         return self.name
+
+
+class Answer(AuthoringModel):
+    status = models.CharField(max_length=127, choices=const.statuses, default=const.IN_PROGRESS)
+    answer = models.TextField(verbose_name='Правильный ответ', null=True, blank=False)
+    mark = models.IntegerField('Максимальный балл')
+    task = models.ForeignKey(to=Task, null=False, on_delete=models.CASCADE)
+    file = models.FileField(verbose_name="Прикрепленный файл", blank=True, null=True, upload_to='media/')
+
+    def __str__(self):
+        return f'{self.answer} ({self.mark})'
