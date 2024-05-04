cae139a57c8f977d0707ff337c15483f7a97a20e
Dsh <knowslana@gmail.com>
2024-05-04 05:08:48 +0300

    попытка сделать сертификат

--- certificate/urls.py
+++ certificate/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('demands/<int:pk>/', DemandListView.as_view(), name="demand-list"),
     path('demand_create/<int:pk>/', DemandCreateView.as_view(), name="demand-create"),
     path('demand_update/<int:pk>/', DemandUpdateView.as_view(), name="demand-update"),
+    path('update_certificate/<int:course_pk>/', update_certificate, name="update_certificate"),
 ]
--- certificate/views.py
+++ certificate/views.py
@@ -1,10 +1,13 @@
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, CreateView, UpdateView
 from django_tables2 import SingleTableView, SingleTableMixin
 
-from certificate.models import Demand
+from certificate import const
+from certificate.generation import gen_document
+from certificate.models import Demand, GeneratedDocument
 from certificate.tables import DemandTable
-from lesson.models import Course
+from lesson.models import Course, CourseRegister
 
 
 class DemandListView(SingleTableMixin, DetailView):
@@ -40,3 +43,21 @@ class DemandUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse_lazy('demand-list', kwargs={'pk': self.object.course.pk})
+
+
+def update_certificate(request, course_pk):
+    profile = request.user.profile
+    course = Course.objects.get(pk=course_pk)
+    has_percent = CourseRegister.objects.get(profile=profile, course=course).percent
+    for demand in course.demand_set.order_by('-percent'):
+        if demand.percent <= has_percent:
+            new_doc = GeneratedDocument.objects.create(
+                demand=demand,
+                status=const.in_progress,
+                course=course,
+                percent=has_percent,
+                author=profile,
+            )
+            gen_document(new_doc.id)
+            GeneratedDocument.objects.filter(author=profile, demand__course=course).exclude(pk=new_doc.pk).delete()
+    raise NotImplemented('Что вернуть?')
